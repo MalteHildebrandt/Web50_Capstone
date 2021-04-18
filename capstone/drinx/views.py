1bbf7693365505bb669fdcc0df9f5eee1796b38e
Malte Hildebrandt <malte.hildebrandt@durstexpress.eu>
2021-04-18 18:29:21 +0200

    initial project setup

---
+++ capstone/drinx/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
+# Create your views here.
+def index(request):
+    message = os.environ['TEST_ENV_VAR']
+    return HttpResponse(message)
